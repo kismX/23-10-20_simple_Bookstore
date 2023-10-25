57ec3e505c6350c79b90e0cc2af8cbcb51df381b
kismX <kism@gmx.net>
2023-10-25 15:09:17 +0200

    test edit

--- books/tests.py
+++ books/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+
+class BookTests(TestCase):
+    def test_url_exists_at_the_correct_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 302)
